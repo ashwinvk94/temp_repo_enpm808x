0d95d63ab3d2e8b834487e8b7a07273ab440f880
Ashwin Varghese Kuruttukulam <ashwinvk94@gmail.com>
2019-12-02 11:55:00 -0500

    [Navigation.hpp][Task 2.3] Updated doxygen comments

--- include/Navigation.hpp
+++ include/Navigation.hpp
@@ -2,7 +2,7 @@
  *  MIT License
  *
  *  Copyright (c) 2019 Rohan Singh, Abhinav Modi, Ashwin Kuruttukulam 
-
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -22,14 +22,15 @@
  * SOFTWARE.
  *******************************************************************************/
 
-/**@file Navigation.hpp
- * @brief Header file for Navigation Module which contains methods for
- *        localization and movement of the robot through the world.
- *
- * Detailed description follows here.
- * @author     : Abhinav Modi
- * @created on : Dec 1, 2019
+/**
+ * @file        Navigation.hpp
+ * @author      Abhinav Modi
+ * @copyright   MIT License (c) 2019 Rohan Singh, Abhinav Modi, Ashwin Kuruttukulam 
+ * @date        Dec 1, 2019
+ * @brief       Header file for Navigation Module which contains methods for
+ *              localization and movement of the robot through the world.
  */
+
 #ifndef INCLUDE_NAVIGATION_HPP_
 #define INCLUDE_NAVIGATION_HPP_
 
@@ -42,31 +43,39 @@
 
 class Navigation {
  public:
-   /**
-    * @brief Callback method for the robot location subscriber  
-    * @param None
-    * @return None
-    */
-   void localizeCb();
+  /**
+   * @brief Callback method for the robot location subscriber  
+   *
+   * @param None
+   *
+   * @return None
+   */
+  void localizeCb();
    
-   /**
-    * @brief Method for publishing goal positions for the motion robot base
-    *        through the environment  
-    * @param None
-    * @return None
-    */
-   void moveBasePub();
+  /**
+   * @brief Method for publishing goal positions for the motion robot base
+   *        through the environment  
+   *
+   * @param pos The desired pose the robot has to be moved to.
+   *
+   * @return None
+   */
+  void moveBasePub(geometry_msgs::PoseStamped pos);
+
+  /**
+   * @brief Method for getting current pose of the robot
+   *
+   * @param None
+   *
+   * @return Returns variable currPos
+   */
+  geometry_msgs::PoseStamped getCurrPose();
     
  private :
-   /**
-    * @brief current Pose of the robot in the world coordinate frame  
-    */
-   geometry_msgs::Pose currPos;
-   
-   /**
-    * @brief 2D grid map of the world for navigation purposes
-    */
-   cv::Mat map; 
+  /**
+   * @brief current Pose of the robot in the world coordinate frame  
+   */
+  geometry_msgs::Pose currPos;
 }
 
 #endif  // INCLUDE_NAVIGATION_HPP_
